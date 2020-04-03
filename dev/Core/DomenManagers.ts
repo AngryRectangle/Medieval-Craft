f25621db9b3e2b18faba8c7702cef906fbf3c5f0
AngryRectangle <yashmel2000@gmail.com>
2020-04-03 15:37:14 +0400

    Create DomenManagers.ts

---
+++ dev/Core/DomenManagers.ts
@@ -0,0 +1,62 @@
+class Domen {
+    public readonly boxes: Geometry.Shapes.Box[];
+    public constructor(boxes: Geometry.Shapes.Box[]){
+        this.boxes = boxes;
+    }
+    public contains(x: number, y: number, z: number): boolean {
+        this.boxes.forEach(box => {
+            if (box.contains(x, y, z, false))
+                return true;
+        });
+        return false;
+    };
+    /*public function contains(point: Geometry.Point): boolean {
+        this.boxes.forEach(box => {
+            if (box.containsPoint(point, false))
+                return true;
+        });
+        return false;
+    }*/
+}
+class DomenManager {
+    domens: Domen[];
+    entities: TileEntity[];
+    public createDomen(entity: TileEntity, boxes: Geometry.Shapes.Box[]): void {
+        this.domens.push(new Domen(boxes));
+        this.entities.push(entity);
+    }
+    public getDomen(x: number, y: number, z: number): Domen {
+        this.domens.forEach(domen => {
+            if (domen.contains(x, y, z))
+                return domen;
+        });
+        return null;
+    }
+    public removeDomenByEntity(entity: TileEntity): boolean {
+        var x: number = entity.x;
+        var y: number = entity.y;
+        var z: number = entity.z;
+        var entity: TileEntity;
+        for (var i = 0; i < this.domens.length; i++) {
+            entity = this.entities[i];
+            if (entity.x == x && entity.y == y && entity.z == z) {
+                this.domens.splice(i, 1);
+                this.entities.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    public removeDomenByCoords(x: number, y: number, z: number): boolean {
+        var domen: Domen;
+        for (var i = 0; i < this.domens.length; i++) {
+            domen = this.domens[i];
+            if (domen.contains(x, y, z)) {
+                this.domens.splice(i, 1);
+                this.entities.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
